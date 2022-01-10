0a735487f9055c43ddd1136f693c95fd170ad8c6
Sungho Lim <sungho.lim@deliveryhero.co.kr>
2022-01-10 22:37:39 +0900

    Static Pre Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <>Home</>;
+  const [counter, setCounter] = useState(0);
+  return <>
+    <h1>Home {counter}</h1>
+    <button onClick={() => setCounter(prev => prev + 1)}>+</button>
+  </>;
 }
 
 export default Home;
